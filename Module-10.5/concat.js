352b2512271e8dde2dd138a0b4abd0181a4dfceb
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-30 05:37:01 +0600

    Concating two arrays and joining the items of any array as a string

---
+++ Module-10.5/concat.js
@@ -0,0 +1,4 @@
+let myArray1 = [23, 45, 67, 89, 74, 32];
+let myArray2 = [69, 96];
+
+console.log(myArray1.concat(myArray2));
---
+++ Module-10.5/joinItem.js
@@ -0,0 +1,8 @@
+const year = [2, 0, 2, 6];
+const joinedYear = year.join('');
+console.log(joinedYear);
+console.log(typeof joinedYear);
+
+let summerFruits = ['Mango', 'Blackberry', 'Jackfruit', 'Lichi', 'Watermelon'];
+let summerFruitsList = summerFruits.join(', ');
+console.log(summerFruitsList);
