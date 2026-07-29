6adfc9e6f47be3607ff9ef8ccc929091e666f215
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-29 20:00:23 +0600

    Array-index and array-length examples

---
+++ Module-10.2/index.js
@@ -0,0 +1,17 @@
+const players = ['Abul', 'Babul', 'Cabul', 'Dabul', 'Kabul'];
+
+console.log('Total No of players:', players.length);
+
+const player1 = players[0];
+const player2 = players[1];
+const player3 = players[2];
+const player4 = players[3];
+const player5 = players[4];
+
+console.log('They are: ');
+console.log(player1);
+console.log(player2);
+console.log(player3);
+console.log(player4);
+console.log(player5);
+
---
+++ Module-10.2/length.js
@@ -0,0 +1,6 @@
+const fruits = ['Mango', 'Lychee', 'JackFruit', 'Papaya', 'Grape', 'Grapefruit'];
+
+const totalFruitsNo = fruits.length;
+
+console.log(totalFruitsNo, 'Kinds of Fruits');
+console.log(fruits);
