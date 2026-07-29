0e4f5f3b6dbb74cb6b1a7da24756906c2d383386
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-29 18:34:05 +0600

    Revision before learning arragy

---
+++ Module-10.1/revision.js
@@ -0,0 +1,21 @@
+/**
+ * 1. Variable
+ * 2. Conditionals
+ * 3. loop:for, while, do-while
+ * 
+ */
+
+
+const price = 50;
+const studentName = 'Zobayer Ahamed';
+const isRich = true;
+
+// if(studentName == 'Talukdar'){
+    
+// }else{
+
+// }
+
+// for (let i = 100; i < 125; i++){
+
+// }
