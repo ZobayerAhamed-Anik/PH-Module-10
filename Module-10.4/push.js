a8647d197b26f3626ce8ac94f5bb0ce2c9414872
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-30 04:28:25 +0600

    pushing a new element to an array

---
+++ Module-10.4/push.js
@@ -0,0 +1,8 @@
+const friends = ['Abul', 'Babul', 'Cabul', 'Dabul', 'Kabul'];
+console.log(friends.length);
+console.log(friends);
+
+friends.push('Mokbul'); // Adds a new element at the end of the array...
+
+console.log(friends.length);
+console.log(friends);
