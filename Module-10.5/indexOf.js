4f1cba2cae6f402a97c3b8403f56dbbff34e4bac
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-30 05:54:21 +0600

    Index of an item which is not included in the array is -1

--- Module-10.5/indexOf.js
+++ Module-10.5/indexOf.js
@@ -1,4 +1,7 @@
 let myArray1 = [23, 45, 67, 89, 74, 32];
-let position = myArray1.indexOf(89);
 
+let position = myArray1.indexOf(89);
 console.log('The position of 89 is', position);
+
+position = myArray1.indexOf(100); // Returns -1, if the item is not inculeded in the array.
+console.log('The position of 100 is', position);
