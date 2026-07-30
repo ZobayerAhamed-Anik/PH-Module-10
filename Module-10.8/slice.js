994501018f8ce406e646826ea1ce15916c72a99a
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-30 15:30:10 +0600

    Slice doesn't change the main array

---
+++ Module-10.8/slice.js
@@ -0,0 +1,6 @@
+const numbers = [23, 45, 26 , 88, 73, 69, 99];
+
+const slicedNumbers = numbers.slice(2,4);
+console.log(slicedNumbers);
+console.log(numbers); // slice() doesn't change the main array
+
