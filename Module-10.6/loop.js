fa4fa712182e9be41390f8a4205ffab17e6b8606
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-30 14:11:18 +0600

    Use of for loop fro traversing item of array

---
+++ Module-10.6/loop.js
@@ -0,0 +1,12 @@
+const numbers = [74, 38, 30, 37, 10, 29, 39];
+
+for(const number of numbers){
+    console.log('Current number is:', number); // traversing through all the numbers in number.
+    // console.log(typeof number);
+}
+
+const players = ['Abul', 'Babul', 'Cabul', 'Dabul', 'Mokbul'];
+
+for(const player of players){
+    console.log('Player Mr.', player);
+}
