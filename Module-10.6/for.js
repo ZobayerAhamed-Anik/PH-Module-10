10b18bc4cd39cb864d80ccac7df1161da53caedd
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-30 14:25:55 +0600

    Using for and while loop to traversing the array items

---
+++ Module-10.6/for.js
@@ -0,0 +1,9 @@
+const numbers = [74, 38, 30, 37, 10, 29, 39];
+
+const length = numbers.length;
+
+for(let i = 0; i < numbers.length; i++){
+    console.log('Index value', i);
+    let item = numbers[i];
+    console.log('Value of the item:', item);
+}
---
+++ Module-10.6/while.js
@@ -0,0 +1,9 @@
+const numbers = [74, 38, 30, 37, 10, 29, 39];
+
+let i = 0;
+while(i < numbers.length ){
+    console.log('Index value', i);
+    let item = numbers[i];
+    console.log('Value of the item:', item);
+    i++;
+}
