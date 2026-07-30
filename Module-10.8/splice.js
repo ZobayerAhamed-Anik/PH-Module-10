b9786a66443e61cf2c763f966063127d906d2b2e
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-30 15:35:04 +0600

    To cut some numbers from the array we use splice(starting index, how much to cut)

---
+++ Module-10.8/splice.js
@@ -0,0 +1,6 @@
+const numbers = [23, 45, 26 , 88, 73, 69, 99];
+console.log(numbers);
+
+const splicedNumbers = numbers.splice(2,3);
+console.log(splicedNumbers);
+console.log(numbers); // splice() cuts the main array
