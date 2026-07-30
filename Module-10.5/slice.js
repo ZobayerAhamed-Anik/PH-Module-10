f2de779efb455a594de95bc80912fd0e8b6ae5d0
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-30 06:01:47 +0600

    slicing an array a fixed range from starting index to ending index

--- Module-10.5/slice.js
+++ Module-10.5/slice.js
@@ -1,4 +1,4 @@
 let myArray1 = [23, 45, 67, 89, 74, 83, 99, 100, 101];
-let slicedArray = myArray1.slice(5,7); //starts after starting index 
+let slicedArray = myArray1.slice(5,7);  
 
 console.log(slicedArray);
