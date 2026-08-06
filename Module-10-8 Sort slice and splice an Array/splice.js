f9d91b25e04f83ff7bb72ec02ce94fd9b6749a12
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-08-06 22:34:05 +0600

    10-8 Sort slice and splice an Array

--- Module-10-8 Sort slice and splice an Array/splice.js
+++ Module-10-8 Sort slice and splice an Array/splice.js
@@ -1,9 +1,9 @@
 const numbers = [23, 45, 26 , 88, 73, 69, 99];
 console.log(numbers);
 
-// const splicedNumbers = numbers.splice(2,3);
-// console.log(splicedNumbers);
-// console.log(numbers); // splice() cuts the main array
+const splicedNumbers = numbers.splice(2,3);
+console.log(splicedNumbers);
+console.log(numbers); // splice() cuts the main array
 
 const editedNumbers = numbers.splice(2, 3, 3, 4, 5);// splicing and replacing with other numbers
 console.log(numbers);
